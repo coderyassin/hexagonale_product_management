2e8a81fded0137ebda22f5ff11ab017ca9b1090e
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-10-12 18:53:02 +0100

    add redis config

--- src/main/java/org/alten/product_management/ProductManagementApplication.java
+++ src/main/java/org/alten/product_management/ProductManagementApplication.java
@@ -3,8 +3,16 @@ package org.alten.product_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		basePackages = "org.alten.product_management",
+		excludeFilters = @ComponentScan.Filter(
+				type = FilterType.REGEX, pattern = "org.alten.product_management.infrastructure.adapter.output.db.mysql.*"
+		)
+)
 @EnableCaching
 public class ProductManagementApplication {
 
